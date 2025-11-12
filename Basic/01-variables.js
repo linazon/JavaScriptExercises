e0f1d7f734e5ef275004eb2770ebbc8e952cfe28
Your Name <you@example.com>
2025-11-12 13:25:07 -0500

    Variables y tipos de datos

---
+++ Basic/01-variables.js
@@ -0,0 +1,21 @@
+// var
+// NUNCA USAR!!!!
+var holaMundoProhibido = "Hola mundo prohibido con var"
+console.log(holaMundoProhibido)
+
+holaMundoProhibido = "Ya ves no me uses!!!"
+console.log(holaMundoProhibido)
+
+// let
+let holaMundoLet ="Este hola mundo si es bonito porque usas let"
+console.log(holaMundoLet)
+
+holaMundoLet = "Hermoso eso modificame"
+console.log(holaMundoLet)
+
+//const
+const constante = "esto nunca canbiara"
+console.log(constante)
+
+constante = "abracadabra cambia ahora!!"
+console.log(constante)
---
+++ Basic/02-dataTypes.js
@@ -0,0 +1,37 @@
+// 7 tipos de datos
+
+// 1. Cadenas de texto (String)
+let nombre = "Lina Katherine"
+let apellido = 'Garzón Pineda'
+
+// 2. Numeros (number)
+let edad = 25
+let codigo = 12.3
+
+// 3. boleanos (Bolean)
+let isStudent = true
+let isTeacher = false
+
+// 4. undefined
+let miIndefinido
+
+// 5. nulo (null)
+let miNulo = null 
+
+// 6. Symbol (simbolo) Usado para objetos
+let mySymbol = Symbol("Este es un simbolo")
+
+// 7. BigInt ( para enteros mayores a 64bit)
+let myBigInt = BigInt(65461654341343214343541324324324134654654321321354321343241)
+
+//Que tipo de dato es cada uno
+console.log(nombre, "es un tipo de dato", typeof(nombre))
+console.log(apellido, "es un tipo de dato", typeof(apellido))
+console.log(edad, "es un tipo de dato", typeof(edad))
+console.log(codigo, "es un tipo de dato", typeof(codigo))
+console.log(isStudent, "es un tipo de dato", typeof(isStudent))
+console.log(isTeacher, "es un tipo de dato", typeof(isTeacher))
+console.log(miNulo, "es un tipo de dato", typeof(miNulo))
+console.log(miIndefinido, "es un tipo de dato", typeof(miIndefinido))
+console.log(mySymbol, "es un tipo de dato", typeof(mySymbol))
+console.log(myBigInt, "es un tipo de dato", typeof(myBigInt))
