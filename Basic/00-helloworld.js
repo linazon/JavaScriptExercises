c27ba3eba373f22a1d9236031a1ded6c5cd9568c
Your Name <you@example.com>
2025-11-11 19:32:54 -0500

    Hola mundo

---
+++ Basic/00-helloworld.js
@@ -0,0 +1,11 @@
+//Esto es un comentario
+
+/*Esto es un comentadio 
+en varias
+lineas*/
+
+console.log("Hola mundo")
+console.log(5 + 2)
+console.log(5 * 2)
+console.log(5 % 2)
+console.log(5 / 2)
